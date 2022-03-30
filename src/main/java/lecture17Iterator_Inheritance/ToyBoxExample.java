49066dd97f2b977fbace5a6766b5b8c5d4d76dbb
Olga Karpenko <okarpenko@usfca.edu>
2022-03-30 09:09:18 -0700

    Iterator/Iterable examples.

---
+++ src/main/java/lecture17Iterator_Inheritance/ToyBoxExample.java
@@ -0,0 +1,22 @@
+package lecture17Iterator_Inheritance;
+
+import java.util.Iterator;
+
+public class ToyBoxExample {
+	public static void main(String[] args) {
+		ToyBox box = new ToyBox();
+		box.add("bear");
+		box.add("car");
+		box.add("lego");
+		box.add("puzzle");
+
+		Iterator<String> it = box.iterator();
+		while (it.hasNext())
+			System.out.println(it.next());
+
+		for (String toy: box) {
+			System.out.println(toy);
+		}
+	}
+
+}
