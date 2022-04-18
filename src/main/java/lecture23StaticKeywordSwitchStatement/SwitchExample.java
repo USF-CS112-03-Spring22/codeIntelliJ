693ddcd3de59dd1c5c482a3ca96c0b109ed81578
Olga Karpenko <okarpenko@usfca.edu>
2022-04-18 09:56:05 -0700

    Added the switch example.

---
+++ src/main/java/lecture23StaticKeywordSwitchStatement/SwitchExample.java
@@ -0,0 +1,26 @@
+package lecture23StaticKeywordSwitchStatement;
+
+import java.util.Scanner;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a month");
+        String month = sc.next();
+        switch (month) {
+            case "January" :
+                System.out.println("Cold!");
+                break;
+            case "February":
+                System.out.println("Spring will be here soon!");
+                break;
+            case "March":
+                System.out.println("ToDo: throw Christmas tree");
+                break;
+            default:
+                System.out.println("The month is " + month);
+                break;
+        }
+
+    }
+}
