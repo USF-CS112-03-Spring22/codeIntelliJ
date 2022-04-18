c1b7619b73d1a68ee6b90934787e1fce097c7704
Olga Karpenko <okarpenko@usfca.edu>
2022-04-18 08:55:37 -0700

    Updated the shapes exercise.

--- src/main/java/lecture22AbstractClasses/shapes/Shape.java
+++ src/main/java/lecture22AbstractClasses/shapes/Shape.java
@@ -1,10 +1,11 @@
 package lecture22AbstractClasses.shapes;
 
-public abstract class Shape {
-    public abstract double computeArea();
+// An abstract super class of classes Circle and Rectangle
+public abstract class Shape {  // note that class is abstract
+    public abstract double computeArea(); // note that the method is abstract
 
     @Override
-    public String toString() {
+    public String toString() { // non-abstract
         return this.getClass().getSimpleName() + ", Area: " + computeArea();
     }
 }
