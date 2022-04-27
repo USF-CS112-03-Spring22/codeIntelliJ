727fd6e016d5b1fc2c4e4f7f65e3647961302d0c
Olga Karpenko <okarpenko@usfca.edu>
2022-04-27 08:35:26 -0700

    Updated recursion examples.

--- src/main/java/lecture26Recursion/RecursionExamples.java
+++ src/main/java/lecture26Recursion/RecursionExamples.java
@@ -2,7 +2,7 @@ package lecture26Recursion;
 
 public class RecursionExamples {
 
-    /** Non-recursively (iterative) factorial
+    /** Non-recursive (iterative) factorial
      *
      * @param n
      * @return factorial of n
@@ -21,28 +21,35 @@ public class RecursionExamples {
      * @return factorial of n
      */
     public static long factorial(int n) {
-        if (n <= 1)
-            return 1; // base case
+        if (n <= 1) // base case
+            return 1;
 
-        else
+        else // recursive case
             return n * factorial(n - 1);
 
     }
 
     public static long sum(int n) {
-
         long result = 0;
         if (n < 0)
             throw new IllegalArgumentException();
 
-        // FILL IN CODE
-        if (n <= 1) {
+        if (n <= 1) { // base case
             return n; // base case
         }
 
 
         result =  n + sum(n-1);
         return result;
+    }
+
+    public int fib(int n) {
+        // FILL IN CODE
+        // Add base cases
+
+        // Add a recursive case
+
+        return 0; // change
     }
 
     /**
@@ -52,14 +59,23 @@ public class RecursionExamples {
      * @return x to the power of n
      */
     public static double pow(double x, int n) {
+        if (n == 0) // base case
+            return 1;
+
         if (n < 0) {
             return (1.0 / x) * pow(x, n + 1);
         }
-        if (n == 0)
-            return 1;
+        else
+            return x * pow(x, n - 1);
 
-        return x * pow(x, n - 1);
+    }
 
+    public static double powAlternativeImplementation(double x, int n) {
+        // FILL IN CODE
+        // if n is even, x^n = x^(n/2) * x^(n/2)
+        // if n is odd, x^n = x * x^(n/2) * x^(n/2)
+
+        return 0; // change
     }
 
     /**
@@ -69,17 +85,24 @@ public class RecursionExamples {
      */
 
     public static String reverse(String s) {
-        // FILL IN CODe
-        if (s.isEmpty())
+        if (s.isEmpty()) // Base case
             return s;
-        //String result  = reverse(s.substring(1)) + s.charAt(0);
-        //return result;
 
+        // Recursive case
         String result = s.charAt(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
         return result;
-
     }
 
+    public static String reverseAlternativeImplementation(String s) {
+        // FILL IN CODE:
+        // Reverse the substring from index 1 until the end of s, and append the first letter of s
+        // Ex: to reverse "crane", we can reverse "rane" to get "enar" and then append a "c" to get
+        // "enarc".
+        if (s.isEmpty()) // base case
+            return s;
+
+        return ""; // change
+    }
     public static boolean isPalindrome(String s) {
         s = s.toLowerCase();
         if (s.isEmpty())
