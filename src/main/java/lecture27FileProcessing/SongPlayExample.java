937bfe17d5ed7214f90202c25ddfe63e1dda3a46
Olga Karpenko <okarpenko@usfca.edu>
2022-05-10 23:51:07 -0700

    Added the method to stop playing the song.

--- src/main/java/lecture27FileProcessing/SongPlayExample.java
+++ src/main/java/lecture27FileProcessing/SongPlayExample.java
@@ -6,6 +6,7 @@ import javazoom.jl.player.Player;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class SongPlayExample {
     public static class PlayerThread extends Thread {
@@ -23,7 +24,6 @@ public class SongPlayExample {
             catch (JavaLayerException e) {
                 e.getMessage();
             }
-
         }
 
         public void run() {
@@ -34,20 +34,34 @@ public class SongPlayExample {
                 e.getMessage();
             }
         }
+
+        public void stopPlay() {
+            player.close();
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         // Without multithreading: do NOT use for the project
-        FileInputStream file = new FileInputStream("input/1.mp3");
+        /*FileInputStream file = new FileInputStream("input/1.mp3");
         try {
             Player player = new Player(file);
             player.play();
+
         } catch (JavaLayerException e) {
             e.printStackTrace();
-        }
+        }*/
 
+        // Note: for this to work, you need to place a file 1.mp3 in the input folder
         // With multithreading - this is what we want to use for the project
-        //Thread helperThread  = new PlayerThread("input/1.mp3");
-        //helperThread.start();
+        PlayerThread helperThread  = new PlayerThread("input/1.mp3");
+        helperThread.start();
+
+        // An example of closing the player to stop playing the song
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Q to stop: ");
+        String command = sc.nextLine();
+        if (command.equals("Q"))
+            helperThread.stopPlay();
+
     }
 }
